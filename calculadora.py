a54542530e0c25e36d1450fb784c3db227dba676
Mateus A. da Silva <79421993+mateusantonioofc@users.noreply.github.com>
2025-09-16 11:11:34 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+while True:
+    num1 = float(input("Digite um numero => "))
+    num2 = float(input("Digite outro numero => "))
+    op = input("Operacao (+, -, *, /) => ").lower()
+
+    if op == "+":
+        print(f'soma: {num1 + num2}')
+    elif op == "-":
+        print(f'subtracao: {num1 - num2}')
+    elif op == "*":
+        print(f'multiplicacao: {num1 * num2}')
+    elif op == "/":
+        if num2 != 0: 
+            print(f'divisao: {num1 / num2}')
+        else:
+            print("Nao pode dividir por 0")
+    else:
+        print("Operacao invalida! Use +, -, * ou /")
